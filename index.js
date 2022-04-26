44187f72fc251232a029c9adb4605c7cf234631d
Facundo Casaccio <casacciofacundo@gmail.com>
2022-04-25 23:08:46 -0300

    Optimizacion, reformateo y ordenamiento de codigo. Aplicacion de sugar syntax. Purga de comentarios innecesarios. Arreglado bug de boton de cambio de modo

--- index.js
+++ index.js
@@ -1,22 +1,180 @@
-let globalArray = []; //array en js que representa el array en pantalla
+//Array mostrado en pantalla
+let globalArray = [];
+
+//***** configuracion *****/
+//Objeto config para uso de JSON y guardar configuraciones de usuario.
 let config = {
-    arrayLength: 30,  //largo del array que se muestra en pantalla
+    arrayLength: 30,
     darkMode: false
 }
 
-//Generar array aleatorio con largo especificado
+//Actualiza la configuracion en local storage
+function updateConfig() {
+    localStorage.setItem("config", JSON.stringify(config));
+}
+
+//Obtiene la configuracion del local storage
+function getConfig() {
+    config = JSON.parse(localStorage.getItem("config"));
+}
+
+//Inicializar configuracion
+function initializeConfig() {
+    //Obtener configuracion local
+    let localConfig = getConfig();
+
+    //Inicializar configuracion default o almacenada en local storage
+    localConfig == undefined ? localStorage.setItem("config", JSON.stringify(config)) : config = localConfig;
+}
+
+//! PROTOCOLOS
+
+//***** Sincronizar select *****//
+function matchSelectedLength() {
+    //Indice de la opcion seleccionada en base a largo 
+    let optionIndex = (config.arrayLength / 10) - 1;
+
+    //Obtener la opcion correspondiente al largo y marcarla como seleccionada
+    let currentLength = document.querySelector("#length").getElementsByTagName("option")[optionIndex];
+    currentLength.selected = true;
+}
+
+//***** Sincronizar tema *****//
+function matchDarkMode() {
+    if (config.darkMode) {
+        //Seleccionar los elementos que por default son light en el html
+        elements = document.querySelectorAll(`.light`);
+        arrayElements = document.querySelectorAll(`.lightArrayElement`);
+
+        //Cambiar clase de elementos
+        elements.forEach(element => element.className = element.className.replace("light", "dark"));
+        arrayElements.forEach(element => element.className = `darkArrayElement`);
+
+        //Actualizar texto de boton
+        document.querySelector("#darkMode").innerHTML = "Light Mode"
+    };
+}
+
+//***** Protocolo de inicio (tareas necesarias al ejecutar) *****//
+function initProtocol() {
+    initializeConfig();
+    matchSelectedLength();
+    globalArray = generateArray();
+    displayArray(globalArray);
+    matchDarkMode();
+}
+
+//***** aplicar modo oscuro *****//
+function darkModeSwitch() {
+    let elements, arrayElements, newClass, currentClass;
+    let button = document.querySelector("#darkMode");
+
+    getConfig();  //obtiene la configuracion actual
+
+    //cambiar las propiedades segun el modo 
+    if (config.darkMode) {
+        currentClass = "dark";
+        newClass = "light";
+        button.innerHTML = "Dark Mode";
+        config.darkMode = false;
+    } else {
+        currentClass = "light";
+        newClass = "dark";
+        button.innerHTML = "Light mode";
+        config.darkMode = true;
+    }
+
+    updateConfig();
+
+    //Seleccionar los elementos
+    elements = document.querySelectorAll(`.${currentClass}`);
+    arrayElements = document.querySelectorAll(`.${currentClass}ArrayElement`);
+
+    //Cambiar tema
+    elements.forEach(element => element.className = element.className.replace(currentClass, newClass));
+    arrayElements.forEach(element => element.className = `${newClass}ArrayElement`);
+}
+
+//! MANIPULACION DE ARRAY EN PANTALLA
+
+//***** Generar array aleatorio *****//
 function generateArray() {
     let array = [];
 
-    //Numero random * 500 para tener la altura y +10 para que la altura no sea 0nunca
     for (let i = 0; i < config.arrayLength; i++) {
         array.push(Math.floor(Math.random() * 500) + 10);
     }
-
     return array;
 }
 
-//Algoritmos de ordenamiento
+//***** Mostrar array en pantalla *****//
+function displayArray(array) {
+    clearArray();
+
+    let list = document.querySelector("#array");
+
+    let className = config.darkMode ? "darkArrayElement" : "lightArrayElement"
+
+    //Crear li por elemento de array y asignarle propiedades para ver en pantalla
+    for (let element of array) {
+        let li = document.createElement("li");
+        li.className = className;
+        li.style = `height: ${element}px`;
+        li.innerHTML = element;
+        list.appendChild(li);
+    }
+}
+
+//***** Borrar array en pantalla *****//
+function clearArray() {
+    document.querySelector("#array").innerHTML = "";
+}
+
+
+// ! EVENTOS
+//***** Generar nuevo array *****/
+document.getElementById("generate").addEventListener("click",
+    () => {
+        getConfig();
+        globalArray = generateArray();
+        displayArray(globalArray)
+    });
+
+//***** seleccionar largo array *****//
+document.querySelector("#length").addEventListener("change", () => {
+    let selectedLenght = document.querySelector("#length").value;
+    config.arrayLength = selectedLenght;
+    updateConfig();
+});
+
+//***** Elegir algoritmo *****//
+document.getElementById("quick").addEventListener("click", () => {
+    globalArray = quickSort(globalArray);
+    displayArray(globalArray);
+});
+
+document.getElementById("bubble").addEventListener("click", () => {
+    globalArray = bubbleSort(globalArray);
+    displayArray(globalArray);
+});
+
+document.getElementById("merge").addEventListener("click", () => {
+    globalArray = mergeSort(globalArray);
+    displayArray(globalArray);
+})
+
+document.getElementById("heap").addEventListener("click", () => {
+    globalArray = heapSort(globalArray);
+    displayArray(globalArray);
+})
+
+//***** activar/desactivar modo oscuro *****//
+document.querySelector("#darkMode").addEventListener("click", () => {
+    darkModeSwitch();
+})
+
+
+//! ALGORITMOS DE ORDENAMIENTO
 
 // ***** QuickSort *****
 function quickSort(array) {
@@ -67,7 +225,7 @@ function heapSort(array) {
     }
 
     //Cambiar el elemento raiz (el mas grande de todos) con el ultimo ordenado (el mas chico)
-    for(let i = array.length - 1; i > 0; i--) {
+    for (let i = array.length - 1; i > 0; i--) {
         swapElements(array, 0, i); //cambiar elementos
         arrayLength--;
         sortParentAndChild(array, arrayLength, 0); //reestructurar el arbol sin el mas grande
@@ -84,13 +242,13 @@ function sortParentAndChild(array, arrayLength, parentIndex) {
     let maxIndex = parentIndex;
 
     //Si existe hijo izquierdo y es mayor, reasignar indice del mayor
-    if(leftIndex < arrayLength && array[leftIndex] > array[maxIndex]) maxIndex = leftIndex;
+    if (leftIndex < arrayLength && array[leftIndex] > array[maxIndex]) maxIndex = leftIndex;
 
     //Si el hijo derecho existe y es mayor, reasignar indice del mayor
-    if(rightIndex < arrayLength && array[rightIndex] > array[maxIndex]) maxIndex = rightIndex;
+    if (rightIndex < arrayLength && array[rightIndex] > array[maxIndex]) maxIndex = rightIndex;
 
     //Si el padre no es el mayor
-    if(maxIndex !== parentIndex) {
+    if (maxIndex !== parentIndex) {
         //intercambiar valores
         swapElements(array, parentIndex, maxIndex);
         //reconstruir estructura de maxHeap
@@ -121,7 +279,7 @@ function mergeSort(array) {
 }
 
 //Unir sub arrays
-function merge (leftArray, rightArray) {
+function merge(leftArray, rightArray) {
     //Contadores de indices de arrays
     let leftIndex = 0;
     let rightIndex = 0;
@@ -146,153 +304,8 @@ function merge (leftArray, rightArray) {
     return [...mergedArray, ...leftArray.slice(leftIndex), ...rightArray.slice(rightIndex)];
 }
 
-//Mostrar array en pantalla
-function displayArray(array) {
-    clearArray(); //limpiar pantalla
-    let list = document.querySelector("#array");
-    let className;
-
-    if (config.darkMode) className = "darkArrayElement"
-    else className = "lightArrayElement"
-
-    //Por cada elemento del array crear un li
-    for(let element of array) {
-        let li = document.createElement("li");
-        li.className = className; //clase con propiedades de los elementos del array
-        li.style = `height: ${element}px`; //estilo priopio de cada elemento (height representa el valor)
-        li.innerHTML = element; //su valor interno para poder manipularlo 
-        list.appendChild(li); //agregar a la lista
-    }
-}
-
-//***** configuracion *****/
-function updateConfig() {
-    localStorage.setItem("config", JSON.stringify(config)); //Actualiza la configuracion modificada
-}
-
-function getConfig() {
-    config = JSON.parse(localStorage.getItem("config")); //Obtiene la configuracion local del usuario
-}
-
-function initializeConfig() { //Inicializar configuracion
-
-    let localConfig = JSON.parse(localStorage.getItem("config"));  //Configuracion local del usuario
-
-    if (localConfig == undefined) localStorage.setItem("config", JSON.stringify(config)) //Inicializar en dafault
-    else config = localConfig; //Inicializar con configuracion local
-}
-
-function matchSelectedLength() { //Iniciar con opcion guardada por usuario
-    
-    let optionIndex = (config.arrayLength / 10) - 1; //Indice de la opcion seleccionada en base a largo 
-    let currentLength = document.querySelector("#length").getElementsByTagName("option")[optionIndex]; //Opcion a seleccionar
-
-    currentLength.selected = true; //Dar atributo de seleccionada para que inicie
-}
-
-function matchDarkMode() { //sincronizar el tema con la configuracion previa del usuario
-    if (config.darkMode) {
-        //Seleccionar los elementos que por default son light en el html
-        elements = document.querySelectorAll(`.light`);
-        arrayElements = document.querySelectorAll(`.lightArrayElement`);
-        
-        //Cambiar clase de elementos
-        elements.forEach( element => element.className = element.className.replace("light", "dark"));
-        arrayElements.forEach( element => element.className = `darkArrayElement`);
-    };
-}
-
-//***** Protocolo de inicio *****//
-function initProtocol() { //protocolo de inicio. Cada vez que se abre hace las tareas necesarias
-    initializeConfig(); //Inicializa configuracion
-    matchSelectedLength(); //Corrige la opcion del select acorde a la longitud del array
-    globalArray = generateArray(); //Genera un array en base a la longitud
-    displayArray(globalArray); //Lo muestra en pantalla
-    matchDarkMode(); //Sincroniza el tema con la configuracion
-}
-
-//Funcionalidad de UI
-//***** aplicar modo oscuro *****//
-function darkModeSwitch() {
-    let elements, arrayElements; //elementos de interfaz y de array
-    let newClass;
-    let currentClass;
-    let button = document.querySelector("#darkMode"); //boton de cambiar modo
-
-    getConfig();  //obtiene la configuracion actual
-
-    //cambiar las propiedades segun el modo 
-    if (config.darkMode) { 
-        currentClass = "dark";
-        newClass = "light";
-        button.innerHTML = "Dark Mode";
-        config.darkMode = false;
-    } else {
-        currentClass = "light";
-        newClass = "dark";
-        button.innerHTML = "Light mode";
-        config.darkMode = true;
-    }
-
-    updateConfig(); //actualizar informacion
-
-    //Seleccionar los elementos segun el tema
-    elements = document.querySelectorAll(`.${currentClass}`);
-    arrayElements = document.querySelectorAll(`.${currentClass}ArrayElement`);
-    
-    //Cambiar clase de elementos
-    elements.forEach( element => element.className = element.className.replace(currentClass, newClass));
-    arrayElements.forEach( element => element.className = `${newClass}ArrayElement`);
-}
-
-//***** Limpiar array en pantalla *****//
-function clearArray() {
-    let list = document.querySelector("#array");
-    list.innerHTML = ""; //borrar los li del array
-}
-
-//***** Generar nuevo array *****/
-document.getElementById("generate").addEventListener("click", 
-    () => { 
-        getConfig();
-        globalArray = generateArray(); //reasignar valor random
-        displayArray(globalArray) //mostrar en pantalla
-    });
-
-//***** seleccionar largo array *****//
-document.querySelector("#length").addEventListener ("change", () => {
-    let selectedLenght = document.querySelector("#length").value;
-    config.arrayLength = selectedLenght;
-    updateConfig();
-});
-
-//***** Elegir algoritmo *****//
-document.getElementById("quick").addEventListener("click", () => {
-    globalArray = quickSort(globalArray); //ordenar
-    displayArray(globalArray); //mostrar
-});
-
-document.getElementById("bubble").addEventListener("click", () => {
-    globalArray = bubbleSort(globalArray); //ordenar
-    displayArray(globalArray); //mostrar
-});
-
-document.getElementById("merge").addEventListener("click", () => { 
-    globalArray = mergeSort(globalArray); //ordenar
-    displayArray(globalArray); //mostrar
-})
-
-document.getElementById("heap").addEventListener("click", () => {
-    globalArray = heapSort(globalArray); //ordenar
-    displayArray(globalArray); //mostrar
-})
-
-//***** activar/desactivar modo oscuro *****//
-document.querySelector("#darkMode").addEventListener("click", () => {
-    darkModeSwitch();
-})
-
-initProtocol(); //Inicializar el programa
+//! INICIO DE PROGRAMA
+initProtocol();
 
 
 
